1f3590594e6ce7e3478b6a98474bf9735f9b7684
busrakk <busrakartal160@gmail.com>
2023-07-16 13:42:20 +0300

    create necessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { ThemeProvider } from "styled-components";
 import { light } from "./styles/Themes";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Home from "./components/sections/Home";
+import About from "./components/sections/About";
+import Roadmap from "./components/sections/Roadmap";
+import Showcase from "./components/sections/Showcase";
+import Team from "./components/sections/Team";
+import Faq from "./components/sections/Faq";
+
 
 function App() {
   return (
     <>
       <ThemeProvider theme={light}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Navigation />
+        <Home />
+        <About />
+        <Roadmap />
+        <Showcase />
+        <Team />
+        <Faq />
+        <Footer />
       </ThemeProvider>
     </>
   );
